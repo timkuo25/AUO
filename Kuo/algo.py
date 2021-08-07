9fe07a3bc873e71bf0163534a70538666b91b560
timkuo25 <b05703100@ntu.edu.tw>
2021-08-07 17:39:01 +0800

    add algo1

---
+++ Kuo/algo.py
@@ -0,0 +1,71 @@
+from read_instance import read_instance
+import time
+
+def algo1_public(m=0, h=0, b=0, n=[], rA=[], rB=[], s=[], p=[], d=[], cD=1, cT=1):
+	c_none = float()
+	sol = []
+	obj = float()
+	candidate = "none"
+	x = [[0]]
+	for i in range(1, m + 1):
+		x.append([0] * (n[i] + 1))
+
+	# defect cost
+	for i in range(1, m + 1):
+		c_none += rB[i] * sum(p[i])
+	c_none *= cD
+	
+	# tardiness
+	for i in range(1, m + 1):
+		for j in range(1, n[i] + 1):
+			tardiness = max(0, s[i][j] + p[i][j] - d[i][j])
+			c_none += cT * tardiness
+	obj = c_none
+	
+	for i in range(1, m + 1):
+		for j in range(1, n[i] + 1):
+			cost = 0
+			
+			for k in range(1, m + 1):
+				if k != i:
+					# defect cost
+					cost += rB[k] * sum(p[k])
+					
+					# tardiness cost
+					tardiness = 0
+					for l in range(1, n[k] + 1):
+						tardiness += max(0, s[k][l] + p[k][l] - d[k][l])
+					cost += cT * tardiness
+		
+				else:
+					# defect cost
+					cost += rB[k] * sum(p[k][:j]) + rA[k] * sum(p[k][j:])
+					
+					# tardiness cost
+					tardiness = 0
+					cur = 0
+					for l in range(1, n[k] + 1):
+						if l == j:
+							cur += b
+						if cur < s[k][l]:
+							cur = s[k][l]
+						cur += p[k][l]
+						tardiness += max(0, cur - d[k][l])
+
+					cost += cT * tardiness
+
+			if cost < obj:
+				obj = cost
+				candidate = (i, j)
+	return obj
+
+# 給雪燕的，只輸出opt和running time	
+def algo1(instance_path):
+	m, h, b, n, rA, rB, s, p, d, cD, cT = read_instance(instance_path)
+	start_time = time.time()
+	opt = algo1_public(m, h, b, n, rA, rB, s, p, d, cD, cT)
+	running_time = time.time() - start_time
+	
+	return opt, running_time
+
+
--- Kuo/read_instance.py
+++ Kuo/read_instance.py
@@ -1,48 +1,41 @@
-m = int()
-h = int()
-b = int()
-n = []
-cD = 1
-cT = 1
+def read_instance(file_path):
+	m = int()
+	h = int()
+	b = int()
+	n = []
+	cD = 1
+	cT = 1
 
-rA = [0]
-rB = [0]
+	rA = [0]
+	rB = [0]
 
-s = [[0]]
-p = [[0]]
-d = [[0]]
-
-file_name = "test.txt"
-with open(file_name, 'r') as f:
-	# m, h, b, n
-	line = f.readline().split()
-	m, h, b = [int(i) for i in line]
-	
-	line = f.readline().split()
-	n = [0] + [int(i) for i in line]
-	
-	# rA, rB
-	for _ in range(m):
+	s = [[0]]
+	p = [[0]]
+	d = [[0]]
+	with open(file_path, 'r') as f:
+		# m, h, b, n
 		line = f.readline().split()
-		rA.append(float(line[1]))
-		rB.append(float(line[2]))
-
-	for i in range(1, 1 + m):
-		s_i = [0]
-		p_i = [0]
-		d_i = [0]
-		for _ in range(n[i]):
+		m, h, b = [int(i) for i in line]
+		
+		line = f.readline().split()
+		n = [0] + [int(i) for i in line]
+		
+		# rA, rB
+		for _ in range(m):
 			line = f.readline().split()
-			s_i.append(int(line[2]))
-			p_i.append(int(line[3]))
-			d_i.append(int(line[4]))
-		s.append(s_i)
-		p.append(p_i)
-		d.append(d_i)
+			rA.append(float(line[1]))
+			rB.append(float(line[2]))
 
-print("n = ", n)
-print("rA = ", rA)
-print("rB = ", rB)
-print("s = ", s)
-print("p = ", p)
-print("d = ", d)
+		for i in range(1, 1 + m):
+			s_i = [0]
+			p_i = [0]
+			d_i = [0]
+			for _ in range(n[i]):
+				line = f.readline().split()
+				s_i.append(int(line[2]))
+				p_i.append(int(line[3]))
+				d_i.append(int(line[4]))
+			s.append(s_i)
+			p.append(p_i)
+			d.append(d_i)
+	return m, h, b, n, rA, rB, s, p, d, cD, cT
