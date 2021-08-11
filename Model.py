07c354126bebab2a06e6595ab4a4f3cad81f2b82
Xin Yu Zhuang <xinyuzhuang@XindeMacBook-Air.local>
2021-08-11 15:32:27 +0800

    update Model.py

--- Model.py
+++ Model.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
 import time
 from gurobipy import *
 
-# t is each minute
 
-
-def Model_1_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
+def Model_eachMin(ma, h, b, n, rA, rB, s, p, d, cD, cT):  # t is each minute
     start = time.time()
     m = Model("research")
     T = 1000
@@ -66,11 +63,12 @@ def Model_1_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
                                       for j in range(n[i]+1)) for i in range(1, ma+1)) <= h)
 
     for (i, j) in X:
-        for t in range(T):
-            if t in range(s[i][j] + p[i][j] + 1, s[i][j] + p[i][j] + b + 1):
-                m.addConstr(A[i, j, t] == 1)
-            else:
-                m.addConstr(A[i, j, t] == 0)
+        if j != 0:
+            for t in range(T):
+                if t in range(s[i][j-1] + p[i][j-1] + 1, s[i][j-1] + p[i][j-1] + b + 1):
+                    m.addConstr(A[i, j, t] == 1)
+                else:
+                    m.addConstr(A[i, j, t] == 0)
 
     m.update()
     m.setObjective(quicksum(p[i][j] * ((rA[i] * Y[i, j]) + rB[i] * (1 - Y[i, j])) for (i, j) in Y)
@@ -94,13 +92,43 @@ def Model_1_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
 
     return opt, model_spend, gap, lowerbound
 
-# t is the period
 
-
-def Model_2_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
+def Model_period(ma, h, b, n, rA, rB, s, p, d, cD, cT):  # t is the period
     start = time.time()
     m = Model("research")
-    T = 1000
+
+    # Starting time preprocessing
+    ss = {}  # Updated starting time
+    TT = []  # T set
+    for i in range(1, ma+1):  # update the starting time if there is the idle time
+        for j in range(n[i], 0, -1):
+            if s[i][j-1] + p[i][j-1] != s[i][j]:
+                ss[i, j] = s[i][j-1] + p[i][j-1]
+                TT.append(ss[i, j])
+            else:
+                ss[i, j] = s[i][j]
+                TT.append(s[i][j])
+
+    last = [s[i][n[i]] + p[i][n[i]] for i in range(1, ma+1)]
+    last.sort()
+    for i in range(len(last)-1):
+        TT.append(last[i])
+    TT = list(set(TT))
+    TT.sort()
+
+    temp = 0
+    ra = {}  # range that includes starting to process to time finished
+    for i in range(1, ma+1):
+        for j in range(1, n[i]+1):
+            valid = 0
+            temp = ss[i, j] + b
+            for k in range(len(TT)):
+                if TT[k] >= temp:
+                    ra[i, j] = (TT.index(ss[i, j]), k)
+                    valid = 1
+                    break
+            if valid == 0:
+                ra[i, j] = (TT.index(ss[i, j]), len(TT)-1)
 
     # Decision Variables
     X = {}
@@ -123,7 +151,7 @@ def Model_2_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
             Z[i, j] = m.addVar(vtype="I", name="Z(%s,%s)" % (i, j))
     for i in range(1, ma+1):
         for j in range(n[i]+1):
-            for t in range(T):
+            for t in range(len(TT)):
                 A[i, j, t] = m.addVar(
                     vtype="B", name="A(%s,%s,%s)" % (i, j, t))
     m.update()
@@ -150,10 +178,17 @@ def Model_2_solve(ma, h, b, n, rA, rB, s, p, d, cD, cT):
         m.addConstr(W[i, j] >= b * quicksum(X[i, k] for k in range(1, j+1)) -
                     quicksum(Y[i, k] * (s[i][k]-s[i][k-1] - p[i][k-1]) for k in range(1, j+1)))
 
-    for t in range(T):
+    for t in range(len(TT)):
         m.addConstr(quicksum(quicksum(A[i, j, t] * X[i, j]
                                       for j in range(n[i]+1)) for i in range(1, ma+1)) <= h)
-    # Difference
+
+    for (i, j) in X:
+        if j != 0:
+            for t in range(len(TT)):
+                if t in range(ra[i, j][0], ra[i, j][1]+1):
+                    m.addConstr(A[i, j, t] == 1)
+                else:
+                    m.addConstr(A[i, j, t] == 0)
 
     m.update()
     m.setObjective(quicksum(p[i][j] * ((rA[i] * Y[i, j]) + rB[i] * (1 - Y[i, j])) for (i, j) in Y)
