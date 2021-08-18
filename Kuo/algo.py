a21089959088d3421c42cc14c968bd07170adc33
timkuo25 <b05703100@ntu.edu.tw>
2021-08-19 06:46:24 +0800

    revised algorithms

--- Kuo/algo.py
+++ Kuo/algo.py
@@ -1,6 +1,6 @@
-from utils import read_instance, feasible, Solution, machine_not_duplicate, feasible_result
+from utils import read_instance, Solution, feasible_result, generate_result, visualize_result, calculate_result_cost
 import time
-from itertools import combinations
+from itertools import product
 import copy
 
 def algo(inst, mode="first_combinations", nth_best=2):
@@ -18,21 +18,18 @@ def algo(inst, mode="first_combinations", nth_best=2):
 	
 	#Parameter manipulation
 	#b = 60
-	cD = 3.8
+	cD = 10
 	#cT = 1
+	h = 2
 	
-	sol = []
-	result = []
-	result_no_maintenance = []
 	collision = []
-	sol_value_dict = {}
-	cost = [[0], [0]]
-	obj = float()
-	c_none_total = 0
-	c_pick_total = 0
+	cost = []
+	first_best_sol = []
 	
 	for i in range(1, m + 1):
-		candidate = None
+		cost_dict = {}
+		candidate = 0
+		best_c = float('inf')
 		c_none = 0
 		c_pick = 0
 
@@ -42,13 +39,14 @@ def algo(inst, mode="first_combinations", nth_best=2):
 		# tardiness
 		for j in range(1, n[i] + 1):
 			c_none += cT * max(0, s[i][j] + p[i][j] - d[i][j])
-			
-		obj = c_none
-		cost[0].append(c_none)
+
+		best_c = c_none
+		cost_dict[0] = c_none
 
 		for j in range(1, n[i] + 1):
 			# defect cost
-			c_pick += rB[i] * sum(p[i][:j]) + rA[i] * sum(p[i][j:])
+			c_pick += cD * (rB[i] * sum(p[i][:j]) + rA[i] * sum(p[i][j:]))
+			
 			# tardiness
 			tardiness = 0
 			cur = 0
@@ -59,180 +57,123 @@ def algo(inst, mode="first_combinations", nth_best=2):
 					cur = s[i][k]
 				cur += p[i][k]
 				tardiness += max(0, cur - d[i][k])
-
+				
 			c_pick += cT * tardiness
+			cost_dict[j] = c_pick
 			
-			if c_pick < obj:
-				obj = c_pick
-				candidate = (i, j)
-		
-		#first best cost
-		cost[1].append(obj)
-		
-		if candidate != None:
-			sol_value_dict[candidate] = obj
-			sol.append(candidate)
+			
+			if c_pick < best_c:
+				best_c = c_pick
+				candidate = j
+				
+		cost.append(cost_dict)
+		first_best_sol.append(candidate)
 	
+	first_best_result = generate_result(s, p, b, first_best_sol)
+	first_best_cost = calculate_result_cost(first_best_result, d, rA, rB, cD, cT)
 	
-	sol_cpy = list(sol)
+	fsb, collision = feasible_result(first_best_result, h)
 	
-	for i in range(1, m + 1):
-		result_i = []
-		cur = 0
-		for j in range(1, n[i] + 1):
-			if cur < s[i][j]: cur = s[i][j]
-			result_i.append((cur, cur + p[i][j], False))
-			cur = cur + p[i][j]
-		result.append(result_i)
+	if fsb: return Solution(s, p, b, first_best_sol, first_best_cost, h, first_best_result, d)
 	
-	result_no_maintenance = copy.deepcopy(result)
-	
-	
-	for i, j in sol:
-		result[i - 1].insert(j - 1, (s[i][j - 1] + p[i][j - 1], s[i][j - 1] + p[i][j - 1] + b, True))
-		cur = s[i][j - 1] + p[i][j - 1] + b
-		
-		for k in range(j, len(result[i - 1])):
-			if cur < result[i - 1][k][0]:
-				cur = result[i - 1][k][0]
-			result[i - 1][k] = (cur, cur + result[i - 1][k][1] - result[i - 1][k][0], False)
-			cur = cur + result[i - 1][k][1] - result[i - 1][k][0]
-	
-	#fsb, collision = feasible_result(result, h)
-	obj_none = sum(cost[0])
-	fsb, collision = feasible(s, p, b, sol, h)
-
-	
-	if fsb:
-		obj = sum(cost[1])
-		solution = Solution(s, p, b, sol, obj, obj_none, h, result_no_maintenance, d=d)
-		return solution
 	
 	if mode == "first_combinations":
-		all_comb = []
-		for i in range(len(sol) - 1):
-			comb = list(combinations(sol, i + 1))
-			for j in comb:
-				j = list(j)
-				all_comb.append(j)
+		list_add_zero = [[i, 0] for i in first_best_sol]
+		candidates = list(set(product(*list_add_zero)))
 		
-		best_obj = float('inf')
 		best_sol = []
-		for comb in all_comb:
-			sol = comb
-			fsb, collision = feasible(s, p, b, sol, h)
-			if fsb:
-				# cost_none
-				sol_cost = sum(cost[0])
-				for i in sol:
-					sol_cost = sol_cost - cost[0][i[0]] + sol_value_dict[i]
-
-				if sol_cost < best_obj:
-					best_obj = sol_cost
-					best_sol = sol
+		best_cost = float('inf')
+		best_result = []
 		
-		solution = Solution(s, p, b, best_sol, best_obj, obj_none, h, result_no_maintenance, d=d)
-		return solution
+		for candidate in candidates:
+			candidate_result = generate_result(s, p, b, list(candidate))
+			fsb, candidate_collision = feasible_result(candidate_result, h)
+			
+			if fsb:
+				candidate_cost = calculate_result_cost(candidate_result, d, rA, rB, cD, cT)
+				if candidate_cost < best_cost:
+					best_cost = candidate_cost
+					best_sol = list(candidate)
+					best_result = candidate_result
+		
+		return Solution(s, p, b, best_sol, best_cost, h, best_result, d)
+		
 		
 	elif mode == "nth_combinations":
-		sol_left = list(sol)
-		for _ in range(1, nth_best):
-			sol_round_n = []
-			cost_n = list(cost[0])
+		list_sorted = [sorted(list(i.items()), key=lambda x: x[1]) for i in cost]
+		list_nth = first_best_sol
+		
+		for i in range(1, nth_best):
+			if i == 1:
+				list_nth = [[list_nth[j]] + [list_sorted[j][i][0]] for j in range(len(list_nth))]
+			else:
+				list_nth = [list_nth[j] + [list_sorted[j][i][0]] for j in range(len(list_nth))]
 			
-			for s_ in sol_left:
-				i = s_[0]
-				obj = cost[0][i] # cost none of machine i
-				prev_best = cost[-1][i]
-				candidate = None
+			candidates = list(set(product(*list_nth)))
+			
+			best_sol = []
+			best_cost = float('inf')
+			best_result = []
+			
+			for candidate in candidates:
+				candidate_result = generate_result(s, p, b, list(candidate))
+				fsb, candidate_collision = feasible_result(candidate_result, h)
 				
-				for j in range(1, n[i] + 1):
-					# defect cost
-					c_pick = rB[i] * sum(p[i][:j]) + rA[i] * sum(p[i][j:])
-				
-					# tardiness
-					tardiness = 0
-					cur = 0
-					for k in range(1, n[i] + 1):
-						if k == j:
-							cur += b
-						if cur < s[i][k]:
-							cur = s[i][k]
-						cur += p[i][k]
-						tardiness += max(0, cur - d[i][k])
-
-					c_pick += cT * tardiness
+				if fsb:
+					candidate_cost = calculate_result_cost(candidate_result, d, rA, rB, cD, cT)
+					if candidate_cost < best_cost:
+						best_cost = candidate_cost
+						best_sol = list(candidate)
+						best_result = candidate_result
 					
-					if c_pick < obj and c_pick > prev_best:
-						obj = c_pick
-						candidate = (i, j)
-			
-				cost_n[i] = obj
-				
-				if candidate != None:
-					sol_value_dict[candidate] = obj
-					sol.append(candidate)
-					sol_round_n.append(candidate)
-			
-			sol_left = sol_round_n
-			cost.append(cost_n)
-			if len(sol_left) == 0: break
-			'''
-			# debug area
-			print(sol)
-			for i in cost:
-				print(i)
-			print(sol_value_dict)
-			'''
+			if  len(best_sol) != 0:
+				return Solution(s, p, b, best_sol, best_cost, h, best_result, d)
 		
-		all_comb = []
-		for i in range(len(sol) - 1):
-			comb = list(combinations(sol, i + 1))
-			for j in comb:
-				j = list(j)
-				all_comb.append(j)
-				
-		all_comb = list(filter(machine_not_duplicate, all_comb))
 		
-		best_obj = float('inf')
+		list_add_zero = [list_nth[j] + [0] for j in range(len(list_nth))]
+		candidates = list(set(product(*list_add_zero)))
+		
 		best_sol = []
-		for comb in all_comb:
-			sol = comb
-			fsb, collision = feasible(s, p, b, sol, h)
+		best_cost = float('inf')
+		best_result = []
+		
+		for candidate in candidates:
+			candidate_result = generate_result(s, p, b, list(candidate))
+			fsb, candidate_collision = feasible_result(candidate_result, h)
+			
 			if fsb:
-				# cost_none
-				sol_cost = sum(cost[0])
-				for i in sol:
-					sol_cost = sol_cost - cost[0][i[0]] + sol_value_dict[i]
-
-				if sol_cost < best_obj:
-					best_obj = sol_cost
-					best_sol = sol
+				candidate_cost = calculate_result_cost(candidate_result, d, rA, rB, cD, cT)
+				if candidate_cost < best_cost:
+					best_cost = candidate_cost
+					best_sol = list(candidate)
+					best_result = candidate_result
 		
-		solution = Solution(s, p, b, best_sol, best_obj, obj_none, h, result_no_maintenance, d=d)
-		return solution
-		
-	elif mode == "strategic_idle":
+		return Solution(s, p, b, best_sol, best_cost, h, best_result, d)
+				
 	
+	elif mode == "waiting":
 		while not fsb:
 			coll_finish_time_list =[]
+			result = first_best_result
+			#visualize_result(first_best_result, first_best_cost, d)
+			
 			
 			for i in collision:
 				coll_finish_time_list.append(result[i[0] - 1][i[1] - 1][1])
+				
+			
 			
 			latest = max(coll_finish_time_list)
 			second_latest = sorted(coll_finish_time_list)[1]
 				
 			latest_machine = [collision[i][0] for i,val in enumerate(coll_finish_time_list) if val==latest]
 			
-
 			move_to_sec = []
 			if len(latest_machine) != len(collision):
 				move_to_sec = latest_machine
 			
 			
 			# calculate cost and decide which to shift
-			
 
 			lowest_cost_increased = float('inf')
 			best_machine_to_shift = None
@@ -299,9 +240,11 @@ def algo(inst, mode="first_combinations", nth_best=2):
 			
 			obj += cT * sum([max(0, filtered_schedule[k][1] - d[i + 1][k + 1]) for k in range(n[i + 1])])
 		
-		solution = Solution(s, p, b, sol, obj, obj_none, h, result_no_maintenance, result=result, d=d)
+		obj = calculate_result_cost(result, d, rA, rB, cD, cT)
+		solution = Solution(s, p, b, sol, obj, h, result=result, d=d)
 		return solution
-	
+		
+		
 		
 # 給雪燕的，只輸出opt和running time	
 def algo_test(instance_path, mode="first_combinations", nth_best=2):
--- Kuo/test.py
+++ Kuo/test.py
@@ -1,23 +1,28 @@
 from algo import algo, algo_test
-from utils import visualize, read_solution, read_instance
-
+from utils import visualize_result, read_solution, read_instance, generate_no_maintenance_result, calculate_result_cost
+import itertools
 
 inst = read_instance("m_6_h_3_n_max_15_1.txt", True)
 
-'''
-sol = algo(inst, "first_combinations")
-visualize(sol)
+#result = generate_no_maintenance_result(inst)
+#obj = calculate_result_cost(result, inst.d, inst.rA, inst.rB, 3.8, inst.cT)
 
-'''
+#print(obj)
+
+r = algo(inst, "nth_combinations", 2)
+visualize_result(r.result, r.obj, r.d)
+
+#print (list(itertools.product([1,2], [0,6], [8, 9], [3, 6])))
 
 '''
 sol = algo(inst, "nth_combinations", 100)
 visualize(sol)
 '''
 
+'''
 sol = algo(inst, "strategic_idle")
 visualize(sol)
-
+'''
 
 
 '''
